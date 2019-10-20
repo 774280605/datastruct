e7f7901f69da368c4414166a5b3e159fafa1a3db
demon <demon@DESKTOP-25UOMB9>
2019-10-20 15:16:37 +0800

    。。。

---
+++ DataStruct/observer/Observer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by demon on 2019/10/20.
+//
+
+#include "Observer.h"
---
+++ DataStruct/observer/Observer.h
@@ -0,0 +1,16 @@
+//
+// Created by demon on 2019/10/20.
+//
+
+#ifndef DATASTRUCT_OBSERVER_H
+#define DATASTRUCT_OBSERVER_H
+
+
+
+class Observer {
+
+};
+
+
+
+#endif //DATASTRUCT_OBSERVER_H
